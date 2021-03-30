cd3df3892d4a4c32358ea171f13f54d12d393f77
TanyaKansal <tanyakansal218@gmail.com>
2021-03-30 12:06:02 +0530

    q136 added mathematical approach based solution also added masking approach in Hamming weight question

---
+++ Easy/SingleNumber.py
@@ -0,0 +1,71 @@
+"""
+
+Given a non-empty array of integers nums, every element appears twice except for one. Find that single one.
+
+Follow up: Could you implement a solution with a linear runtime complexity and without using extra memory?
+
+
+
+Example 1:
+
+Input: nums = [2,2,1]
+Output: 1
+
+Example 2:
+
+Input: nums = [4,1,2,1,2]
+Output: 4
+
+Example 3:
+
+Input: nums = [1]
+Output: 1
+
+
+
+Constraints:
+
+    1 <= nums.length <= 3 * 104
+    -3 * 104 <= nums[i] <= 3 * 104
+    Each element in the array appears twice except for one element which appears only once.
+
+
+"""
+
+from typing import List
+
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        element_count_dict = {}
+        for num in range(0, len(nums)):
+            if nums[num] in element_count_dict:
+                element_count_dict[nums[num]] += 1
+            else:
+                element_count_dict[nums[num]] = 1
+
+        for key, value in element_count_dict.items():
+            if value == 1:
+                return key
+
+
+    def mathematical_approach(self,nums: List[int]) -> int:
+
+        """
+
+        :param nums:
+        :return:
+
+        2∗(a+b+c)−(a+a+b+b+c)=c2 * (a + b + c) - (a + a + b + b + c) = c2∗(a+b+c)−(a+a+b+b+c)=c
+
+
+        does not work for the case where the occurrence for the numbers is more thsn 2
+        """
+        return 2*sum(set(nums)) - sum(nums)
+
+
+obj=Solution()
+print(obj.singleNumber([2,2,1]))
+
+
+
+print(obj.mathematical_approach([2,2,1]))
