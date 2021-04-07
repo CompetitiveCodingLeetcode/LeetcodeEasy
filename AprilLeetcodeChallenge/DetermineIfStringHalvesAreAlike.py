8145f7d5e248e2cfc1988e3d6b78997a103cf342
TanyaKansal <tanyakansal218@gmail.com>
2021-04-07 22:18:37 +0530

    Determine if String Halves Are Alike ---> q1704 easy

---
+++ AprilLeetcodeChallenge/DetermineIfStringHalvesAreAlike.py
@@ -0,0 +1,65 @@
+"""
+You are given a string s of even length. Split this string into two halves of equal lengths, and let a be the first half and b be the second half.
+
+Two strings are alike if they have the same number of vowels ('a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'). Notice that s contains uppercase and lowercase letters.
+
+Return true if a and b are alike. Otherwise, return false.
+
+
+
+Example 1:
+
+Input: s = "book"
+Output: true
+Explanation: a = "bo" and b = "ok". a has 1 vowel and b has 1 vowel. Therefore, they are alike.
+
+Example 2:
+
+Input: s = "textbook"
+Output: false
+Explanation: a = "text" and b = "book". a has 1 vowel whereas b has 2. Therefore, they are not alike.
+Notice that the vowel o is counted twice.
+
+Example 3:
+
+Input: s = "MerryChristmas"
+Output: false
+
+Example 4:
+
+Input: s = "AbCdEfGh"
+Output: true
+
+
+
+Constraints:
+
+    2 <= s.length <= 1000
+    s.length is even.
+    s consists of uppercase and lowercase letters.
+
+
+"""
+
+class Solution:
+    def halvesAreAlike(self, s: str) -> bool:
+        length_s=len(s)
+        # print(length_s)
+        first_half_vowel_count = second_half_vowel_count = 0
+        for i in range(0,length_s//2):
+            # print(s[i])
+            # print("___",s[length_s-1-i])
+            if s[i] in ['a','e','i','o','u','A','E','I','O','U']:
+                first_half_vowel_count += 1
+                # print(first_half_vowel_count)
+            if s[length_s-1-i] in ['a','e','i','o','u','A','E','I','O','U']:
+                second_half_vowel_count +=1
+                # print(second_half_vowel_count)
+        if first_half_vowel_count == second_half_vowel_count:
+            return True
+        else:
+            return False
+
+obj=Solution()
+print(obj.halvesAreAlike("AbCdEfGh"))
+print(obj.halvesAreAlike("MerryChristmas"))
