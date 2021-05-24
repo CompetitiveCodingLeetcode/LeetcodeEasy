7396490e04a5aa8dd24c67a3d0f523032e2865f6
TanyaKansal <tanyakansal218@gmail.com>
2021-05-24 19:40:11 +0530

    time analysis

--- MayLeetcodeChallenge/ToLowerCase_Q709.py
+++ MayLeetcodeChallenge/ToLowerCase_Q709.py
@@ -1,13 +1,23 @@
+import time
+
 class Solution:
     def toLowerCase(self, s: str) -> str:
+        start=time.time()
         new_s=""
+        # new_s= "".join(chr(ord(ch)+32) if 'A'<=ch<='Z' else ch for ch in s)
         for ch in s:
             if 65<=ord(ch)<=90:
                 # ord -> integer representation of character |  chr -> character representation of integer
                 new_s+=chr(ord(ch)+32)
             else:
                 new_s+=ch
+        end=time.time()
+        print("time taken=",end-start)
         return new_s
 
 obj=Solution()
-print(obj.toLowerCase("heLLo"))
+
+# 7.486343383789062e-05 ---- commented approach time
+# 6.413459777832031e-05 --- current approach time
+
+print(obj.toLowerCase("heLLovkzsfbjsbegrjsgbsjdbjabfbglsdbvljsgenfkKJSBRGRNWGKSBVJADBVLBAAGKRNGJDBflrnsbjvbgslgbA"))
