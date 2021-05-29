ad8aaaf765517a54ea20ce6e8ece8fc7da940327
TanyaKansal <tanyakansal218@gmail.com>
2021-05-29 22:54:11 +0530

    add sort approach and its test cases

--- Lists/findMaxProductOfTwoNumsInArray.py
+++ Lists/findMaxProductOfTwoNumsInArray.py
@@ -1,7 +1,7 @@
 import unittest
 from typing import List
 
-
+# Time complexity=O(n^2)
 def find_max_product_bruteforce_approach(nums: List[int]) -> int:
     max_product = 0
     for i in range(len(nums)):
@@ -11,6 +11,16 @@ def find_max_product_bruteforce_approach(nums: List[int]) -> int:
 
     return max_product
 
+# time complexity=O(nlogn)
+def find_max_product_sort_approach(nums: List[int]) -> int:
+    nums.sort()
+    max_product = nums[0] * nums[1]
+
+    # handling the product of 2 negative integers if greater than the max_product as the negative numbers while sorting will come in the end of array
+    if nums[len(nums) - 1] * nums[len(nums) - 2] > max_product:
+        max_product = nums[len(nums) - 1] * nums[len(nums) - 2]
+    return max_product
+
 
 class TestSolution(unittest.TestCase):
     def test_all_positive_elements(self):
@@ -18,3 +28,10 @@ class TestSolution(unittest.TestCase):
 
     def test_negative_elements(self):
         self.assertEqual(40, find_max_product_bruteforce_approach([1, 2, -8, -3, -5]))
+
+    def test_sort_approach_all_positive_elements(self):
+        self.assertEqual(40, find_max_product_sort_approach([1, 2, 8, 3, 5]))
+
+    def test_sort_approach_negative_elements(self):
+        self.assertEqual(40, find_max_product_sort_approach([1, 2, -8, -3, -5]))
+
