ac358d7f88503440bb7f0064eb77d383538c2b80
TanyaKansal <tanyakansal218@gmail.com>
2021-06-02 11:36:10 +0530

    added functions for inserting and traversing a linked list

--- LinkedList/CreateLinkedList.py
+++ LinkedList/CreateLinkedList.py
@@ -12,27 +12,27 @@ class Node:
 class SingleLinkedList:
     def __init__(self):
         self.head = None
-        # self.tail = None
+
+    def insert(self,n):
+        temp = self.head
+
+        for i in range(n):
+            node = Node(i)
+            if single_linked_list_obj.head == None:
+                temp = node
+                single_linked_list_obj.head = temp
+            else:
+                temp.next = node
+                temp = node
+
+    def traverse(self):
+        temp1 = self.head
+        while temp1:
+            print("Inside")
+            print(temp1.val)
+            temp1 = temp1.next
 
 
 single_linked_list_obj = SingleLinkedList()
-temp = single_linked_list_obj.head
-
-
-for i in range(4):
-    node = Node(i)
-    if single_linked_list_obj.head == None:
-        print("Inserting first node")
-        temp = node
-        single_linked_list_obj.head = temp
-    else:
-        temp.next = node
-        temp = node
-
-
-temp1 = single_linked_list_obj.head
-print("temp1=",temp1)
-for i in range(4):
-        print("Inside")
-        print(temp1.val)
-        temp1= temp1.next
+single_linked_list_obj.insert(5)
+single_linked_list_obj.traverse()
