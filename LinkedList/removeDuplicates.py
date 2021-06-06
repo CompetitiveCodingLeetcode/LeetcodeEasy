2956cac2a32990699d4f1dfb751ab7144611d83d
TanyaKansal <tanyakansal218@gmail.com>
2021-06-06 19:50:26 +0530

    typo corrections

--- LinkedList/removeDuplicates.py
+++ LinkedList/removeDuplicates.py
@@ -1,7 +1,8 @@
 from LinkedList import LinkedList
 
+
 # remove duplicates in unsorted linked list
-#this approach uses a temporary buffer hence Space complexity = O(n) and time complexity is O(n)
+# this approach uses a temporary buffer hence Space complexity = O(n) and time complexity is O(n)
 # if temporary buffer is not to be used then Time complexity will be O(n^2)
 
 def remove_dups(linked_list):
@@ -9,21 +10,21 @@ def remove_dups(linked_list):
     if temp is None:
         return
     else:
-        visited_nodes = {}
-        visited_nodes[temp.val] = 1
+        visited_nodes = {temp.val: 1}
         while temp.next:
             if temp.next.val in visited_nodes:
                 temp.next = temp.next.next
             else:
-                visited_nodes[temp.next.val]=1
+                visited_nodes[temp.next.val] = 1
                 temp = temp.next
         return linked_list
 
+
 linked_list = LinkedList()
-linked_list.generate_list(10,0,99)
+linked_list.generate_list(10, 0, 99)
 print(linked_list)
 remove_dups(linked_list)
 print(linked_list)
 
-#TODO: remove duplicates from sorted linked list
-#TODO: sort linked list using merge sort
+# TODO: remove duplicates from sorted linked list
+# TODO: sort linked list using merge sort
