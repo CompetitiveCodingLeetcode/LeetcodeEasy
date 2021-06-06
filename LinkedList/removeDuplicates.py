d8dba303a84d2f05b24eaa3c5094865a4ed71a74
TanyaKansal <tanyakansal218@gmail.com>
2021-06-06 18:09:05 +0530

    remove duplicates in a linked list using O(n)

---
+++ LinkedList/removeDuplicates.py
@@ -0,0 +1,26 @@
+from LinkedList import LinkedList
+
+# remove duplicates in unsorted linked list
+def remove_dups(linked_list):
+    temp = linked_list.head
+    if temp is None:
+        return
+    else:
+        visited_nodes = {}
+        visited_nodes[temp.val] = 1
+        while temp.next:
+            if temp.next.val in visited_nodes:
+                temp.next = temp.next.next
+            else:
+                visited_nodes[temp.next.val]=1
+                temp = temp.next
+        return linked_list
+
+linked_list = LinkedList()
+linked_list.generate_list(10,0,99)
+print(linked_list)
+remove_dups(linked_list)
+print(linked_list)
+
+#TODO: remove duplicates from sorted linked list
+#TODO: sort linked list using merge sort
