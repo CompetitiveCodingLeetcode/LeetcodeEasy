5465d9ddc592ae55b903046eea790ee53ad9e6b1
TanyaKansal <tanyakansal218@gmail.com>
2021-06-08 21:09:54 +0530

    use lru cache instead of using external dict

--- DynamicProgramming/fibonacciUsingMemoization.py
+++ DynamicProgramming/fibonacciUsingMemoization.py
@@ -1,14 +1,16 @@
-fibonacci_store = {}
+from functools import lru_cache
 
+# fibonacci_store = {}
+@lru_cache(1000)
 def fibonacci(n):
-    if n in fibonacci_store:
-        print(fibonacci_store[n],end=" ")
-        return fibonacci_store[n]
+    # if n in fibonacci_store:
+    #     print(fibonacci_store[n],end=" ")
+    #     return fibonacci_store[n]
     if n in [1,2]:
         return 1
     value = fibonacci(n-1) + fibonacci(n-2)
     # print(value, end=" ")
-    fibonacci_store[n] = value
+    # fibonacci_store[n] = value
     return value
 
 print(fibonacci(4))
