de0e8c687218e4324fffcbc4bc9acc6f8f664a9d
TanyaKansal <tanyakansal218@gmail.com>
2021-06-12 12:59:27 +0530

    add first solution for alternate reversal of linked list ques asked in amazon

---
+++ LinkedList/AlternateReversalOfLinkedListInGroupOfSizeK.py
@@ -0,0 +1,50 @@
+"""
+Alternate reversal of linked list in a group of size k.
+Like the first group should be reversed, the second group should be the same, the third should be reversed, the fourth should be the same, etc.
+Example :
+
+Input : 1->2->3->4->5->6->7->8,  k=2
+Output : 2->1->3->4->6->5->7->8
+
+"""
+
+from LinkedList import LinkedList
+
+
+def traverse_and_store(input_LL, k):
+    temp = input_LL.head
+    input_LL_map = {}
+    count = 0
+
+    for i in range(int(len(input_LL) / k)):
+        group_val_list = []
+        while count != k:
+            group_val_list.append(temp.val)
+            count += 1
+            temp = temp.next
+        if i % 2 == 0:
+            group_val_list.reverse()
+            input_LL_map[i] = group_val_list
+        else:
+            input_LL_map[i] = group_val_list
+        count = 0
+    return input_LL_map
+
+
+def set_values_for_linked_list(input_LL, k):
+    temp = input_LL.head
+    input_LL_map = traverse_and_store(input_LL, k)
+    count = 0
+    for i in range(int(len(input_LL) / k)):
+        while count != k:
+            temp.val = input_LL_map[i][count]
+            temp = temp.next
+            count += 1
+        count = 0
+
+
+customLL = LinkedList()
+customLL.generate_list(12, 0, 99)
+print(customLL)
+set_values_for_linked_list(customLL, 3)
+print(customLL)
