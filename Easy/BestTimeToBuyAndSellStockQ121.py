ea2487cabb51b978ac2909d82b11bc5bdc65942f
Kansal <kansal@Kansals-MacBook-Pro.local>
2022-02-05 13:18:36 +0530

    add optimized approach for q121

--- Easy/BestTimeToBuyAndSellStockQ121.py
+++ Easy/BestTimeToBuyAndSellStockQ121.py
@@ -30,7 +30,7 @@ from typing import List
 
 
 class Solution:
-    #O(n^2) approach
+    #O(n^2) approach  --- TLE will come as per constraints
     def maxProfit_approach1(self, prices: List[int]) -> int:
         max_profit = 0
         max_price = 0
@@ -41,6 +41,36 @@ class Solution:
                 if max_price - price > max_profit:
                     max_profit = max_price - price
 
+        return max_profit
+
+
+    # O(n) approach
+    """
+    Approach 2: One Pass
+Algorithm
+
+Say the given array is:
+
+[7, 1, 5, 3, 6, 4]
+
+If we plot the numbers of the given array on a graph, we get:
+
+Profit Graph
+
+The points of interest are the peaks and valleys in the given graph. We need to find the largest peak following the smallest valley.
+ We can maintain two variables - minprice and maxprofit corresponding to the smallest valley and maximum profit (maximum difference between selling price and minprice) obtained so far respectively.
+ 
+ 
+ NOTE: What we are actually doing is this: for every element, we are calculating the difference between that element and the minimum of all the values before that element and we are updating the maximum profit if the difference thus found is greater than the current maximum profit.
+    """
+    def maxProfit_approach2(self, prices: List[int]) -> int:
+        min_price = max(prices)
+        max_profit = 0
+        for price in prices:
+            if price < min_price:
+                min_price = price
+            elif price - min_price > max_profit:
+                max_profit = price - min_price
         return max_profit
 
 
@@ -55,4 +85,13 @@ class TestSolution(unittest.TestCase):
         self.assertEqual(self.obj.maxProfit_approach1([7,6,4,3,1]),0)
 
     def test_case3_approach1(self):
+        self.assertEqual(self.obj.maxProfit_approach1([2,4,1]),2)
+
+    def test_case1_approach2(self):
+        self.assertEqual(self.obj.maxProfit_approach1([7,1,5,3,6,4]),5)
+
+    def test_case2_approach2(self):
+        self.assertEqual(self.obj.maxProfit_approach1([7,6,4,3,1]),0)
+
+    def test_case3_approach2(self):
         self.assertEqual(self.obj.maxProfit_approach1([2,4,1]),2)
