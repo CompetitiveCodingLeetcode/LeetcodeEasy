d4d9a74a988bc60f6ea099a76b1cfc84e3c6b98d
Kansal <kansal@Kansals-MacBook-Pro.local>
2022-02-16 18:40:26 +0530

    add approach 2 description

--- Medium/GroupAnagrams_Q49.py
+++ Medium/GroupAnagrams_Q49.py
@@ -43,6 +43,17 @@ class Solution:
         return ans
 
     # Time complexity: O(nk) and space complexity: O(nk)
+    """
+    Intuition
+
+Two strings are anagrams if and only if their character counts (respective number of occurrences of each character) are the same.
+
+Algorithm
+
+We can transform each string \text{s}s into a character count, \text{count}count, consisting of 26 non-negative integers representing the number of \text{a}a's, \text{b}b's, \text{c}c's, etc. We use these counts as the basis for our hash map.
+
+In Java, the hashable representation of our count will be a string delimited with '#' characters. For example, abbccc will be #1#2#3#0#0#0...#0 where there are 26 entries total. In python, the representation will be a tuple of the counts. For example, abbccc will be (1, 2, 3, 0, 0, ..., 0), where again there are 26 entries total.
+    """
     def groupAnagrams_approach2(self,strs: List[str]) -> List[List[str]]:
         ans = collections.defaultdict(list)
         res = []
