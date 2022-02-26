03e40ccdf7bdaf8c4804cb70a5d18c9384263839
Kansal <kansal@Kansals-MacBook-Pro.local>
2022-02-26 15:19:25 +0530

    optimized approach for q69

--- Searching/BinarySearch/Sqrt_Q69.py
+++ Searching/BinarySearch/Sqrt_Q69.py
@@ -23,6 +23,7 @@ Constraints:
 0 <= x <= 231 - 1
 """
 import unittest
+from math import e, log
 
 
 class Solution:
@@ -44,6 +45,24 @@ class Solution:
             mid = low + int((high - low)/2)
         return ans
 
+
+    #O(1) approach to find sqrt
+    """
+        Before going to the serious stuff, let's first have some fun and implement the algorithm used by the pocket calculators.
+
+        Usually a pocket calculator computes well exponential functions and natural logarithms by having logarithm tables hardcoded or by the other means. Hence the idea is to reduce the square root computation to these two algorithms as well
+        sqrt(x) = e**(0.5 * log(x))
+        
+    """
+
+    def mySqrt_approach2(self, x):
+        if x < 2:
+            return x
+
+        left = int(e ** (0.5 * log(x)))
+        right = left + 1
+        return left if right * right > x else right
+
 class TestSolution(unittest.TestCase):
     def setUp(self) -> None:
         self.obj = Solution()
@@ -59,3 +78,15 @@ class TestSolution(unittest.TestCase):
 
     def test_case4(self):
         self.assertEqual(self.obj.mySqrt(64),8)
+
+    def test_case1_approach2(self):
+        self.assertEqual(self.obj.mySqrt_approach2(6),2)
+
+    def test_case2_approach2(self):
+        self.assertEqual(self.obj.mySqrt_approach2(0),0)
+
+    def test_case3_approach2(self):
+        self.assertEqual(self.obj.mySqrt_approach2(1),1)
+
+    def test_case4_approach2(self):
+        self.assertEqual(self.obj.mySqrt_approach2(64),8)
