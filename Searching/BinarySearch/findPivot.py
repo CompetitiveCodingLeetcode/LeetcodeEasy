81b5f135283310ef3871986800613baee12b902e
Kansal <kansal@Kansals-MacBook-Pro.local>
2022-02-26 12:08:27 +0530

    add application of binary search

---
+++ Searching/BinarySearch/findPivot.py
@@ -0,0 +1,32 @@
+"""
+in an array [3,4,5,6,1] , pivot is the index at which min element in array occurs.
+"""
+import unittest
+from typing import List
+
+
+class Solution:
+    def find_pivot(self,nums: List[int]) -> int:
+        s = 0
+        e = len(nums) -1
+        mid = s + int((e-s)/2)
+        while s< e:
+            if nums[mid] >= nums[0]:
+                s = mid+1
+            else:
+                e = mid
+            mid = s + int((e-s)/2)
+        return s
+
+class TestSolution(unittest.TestCase):
+    def setUp(self) -> None:
+        self.obj = Solution()
+
+    def test_case1(self):
+        self.assertEqual(self.obj.find_pivot([6,7,1,2,3]),2)
+
+    def test_case2(self):
+        self.assertEqual(self.obj.find_pivot([6,7,8,1,2]),3)
+
+    def test_case3(self):
+        self.assertEqual(self.obj.find_pivot([7,1,2,3,4]),1)
