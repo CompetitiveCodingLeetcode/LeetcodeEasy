82835e91d18afc88a6da66872bf9815cbd169053
Kansal <kansal@Kansals-MacBook-Pro.local>
2022-03-22 17:09:30 +0530

    add solution to q1910

---
+++ Medium/RemoveAllOccurrencesOfASubstring_Q1910.py
@@ -0,0 +1,62 @@
+"""
+Given two strings s and part, perform the following operation on s until all occurrences of the substring part are removed:
+
+Find the leftmost occurrence of the substring part and remove it from s.
+Return s after removing all occurrences of part.
+
+A substring is a contiguous sequence of characters in a string.
+
+
+
+Example 1:
+
+Input: s = "daabcbaabcbc", part = "abc"
+Output: "dab"
+Explanation: The following operations are done:
+- s = "daabcbaabcbc", remove "abc" starting at index 2, so s = "dabaabcbc".
+- s = "dabaabcbc", remove "abc" starting at index 4, so s = "dababc".
+- s = "dababc", remove "abc" starting at index 3, so s = "dab".
+Now s has no occurrences of "abc".
+Example 2:
+
+Input: s = "axxxxyyyyb", part = "xy"
+Output: "ab"
+Explanation: The following operations are done:
+- s = "axxxxyyyyb", remove "xy" starting at index 4 so s = "axxxyyyb".
+- s = "axxxyyyb", remove "xy" starting at index 3 so s = "axxyyb".
+- s = "axxyyb", remove "xy" starting at index 2 so s = "axyb".
+- s = "axyb", remove "xy" starting at index 1 so s = "ab".
+Now s has no occurrences of "xy".
+
+
+Constraints:
+
+1 <= s.length <= 1000
+1 <= part.length <= 1000
+s and part consists of lowercase English letters.
+"""
+import unittest
+
+
+class Solution:
+    def removeOccurrences(self, s: str, part: str) -> str:
+        new_s = ""
+        i=0
+        while i< len(s):
+            new_s += s[i]
+            if part in new_s:
+                new_s = new_s.replace(part,"")
+            i+=1
+        return new_s
+
+
+class TestSolution(unittest.TestCase):
+    def setUp(self) -> None:
+        self.obj = Solution()
+
+    def test_case1(self):
+        self.assertEqual(self.obj.removeOccurrences("daabcbaabcbc","abc"),"dab")
+
+    def test_case2(self):
+        self.assertEqual(self.obj.removeOccurrences("axxxxyyyyb","xy"),"ab")
+
