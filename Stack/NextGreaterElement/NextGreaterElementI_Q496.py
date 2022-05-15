1894b893470e172314c115a6540695e6f5425a00
Kansal <kansal@Kansals-MacBook-Pro.local>
2022-05-15 07:37:55 +0530

    add appproach for q496

--- Stack/NextGreaterElement/NextGreaterElementI_Q496.py
+++ Stack/NextGreaterElement/NextGreaterElementI_Q496.py
@@ -36,6 +36,22 @@ All the integers of nums1 also appear in nums2.
 
 Follow up: Could you find an O(nums1.length + nums2.length) solution?
 
+
+APPROACH:
+Algorithm˙
+
+In this approach, we make use of pre-processing first so as to make the results easily available later on. We make use of a stack (stack) and a hashmap (map). map is used to store the result for every posssible number in nums2 in the form of (element,next_greater_element). Now, we will look at how to make entries in map.
+
+We iterate over the nums2 array from the left to right. We push every element nums2[i] on the stack if it is less than the previous element on the top of the stack (stack[top]). No entry is made in map for such nums2[i]'s right now. This happens because the nums2[i]'s encountered so far are coming in a descending order.
+
+If we encounter an element nums2[i] such that nums2[i] > stack[top], we keep on popping all the elements from stack[top] until we encounter stack[k] such that stack[k] ≥ nums2[i]. For every element popped out of the stack stack[j], we put the popped element along with its next greater number (result) into the hashmap map, in the form (stack[j], nums2[i]). Now, the next greater element for all elements stack[j], such that k < j ≤ toptop is nums2[i] (since this larger element caused all the stack[j]'s to be popped out). We stop popping the elements at stack[k] because this nums2[i] can't act as the next greater element for the next elements on the stack.
+
+Thus, an element is popped out of the stack whenever a next greater element is found for it. Therefore, the elements remaining in the stack are the ones for which no next greater element exists in the nums2 array. Thus, at the end of the iteration over nums2, we pop the remaining elements from the stack and put their entries in hash with a \text{-1}-1 as their corresponding results.
+
+Then, we can simply iterate over the nums1nums1 array to find the corresponding results from map directly.
+
+
+
 """
 import unittest
 from typing import List
