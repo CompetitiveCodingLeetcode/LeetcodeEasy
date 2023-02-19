e3b30a5daf4885cd1f83a0a3c47ed9a8bd5fa4d0
TanyaKansal <tanyakansal218@gmail.com>
2023-02-19 09:47:35 -0800

    add heapify logic and correct deletion logic

--- Heap/heap.py
+++ Heap/heap.py
@@ -1,10 +1,11 @@
 class Heap:
-    def __init__(self):
-        self.heap_arr = [None]
-        self.heap_size = 0
+    def __init__(self,arr):
+        self.heap_arr = arr
+        self.heap_size = len(self.heap_arr)
+
 
     def print_heap(self):
-        for i in range(1,self.heap_size+1):
+        for i in range(1,len(self.heap_arr)):
             print(self.heap_arr[i],end=",")
         print("\n")
 
@@ -22,9 +23,9 @@ class Heap:
         """
         self.heap_size += 1
         self.heap_arr.append(val)
-        if self.heap_size == 1:
+        if self.heap_size == 2:
             return
-        idx = self.heap_size
+        idx = self.heap_size-1
         parent_idx = idx // 2
 
         while parent_idx > 0:
@@ -40,9 +41,9 @@ class Heap:
 
         self.heap_size += 1
         self.heap_arr.append(val)
-        if self.heap_size == 1:
+        if self.heap_size == 2:
             return
-        idx = self.heap_size
+        idx = self.heap_size -1
         parent_idx = idx//2
 
         while parent_idx > 0:
@@ -59,57 +60,88 @@ class Heap:
         Algo:
         1. put last element as root
         2. delete last element
-        3. place the element at root at correct place:
+        3. place the element at root at correct place(heapify):
             a. if : compare root with left child if does not satisfy max heap property, then swap. continue till current idx < size of heap.
             b. elif: compare root with right child if does not satisfy max heap property, then swap. continue till current idx < size of heap.
             c. else return
         """
-        if self.heap_size == 0:
+        if self.heap_size == 1:
             print("nothing to delete")
-        self.heap_arr[1] = self.heap_arr[self.heap_size]
+        self.heap_arr[1] = self.heap_arr[self.heap_size-1]
         self.heap_arr.pop()
         self.heap_size -= 1
 
-        idx = 1
-        while idx<self.heap_size:
-            left_idx = 2*idx
-            right_idx = 2*idx + 1
-            if left_idx <= self.heap_size and self.heap_arr[left_idx] > self.heap_arr[idx]:
-                self.swap(left_idx,idx)
-                idx = left_idx
-            elif right_idx <= self.heap_size and self.heap_arr[right_idx] > self.heap_arr[idx]:
-                self.swap(right_idx,idx)
-                idx = right_idx
-            else:
-                return
+        for i in range(self.heap_size//2,0,-1):
+            self.heapify_max_heap(self.heap_size,i)
 
+        # idx = 1
+        # while idx<self.heap_size:
+        #     left_idx = 2*idx
+        #     right_idx = 2*idx + 1
+        #     if left_idx < self.heap_size and self.heap_arr[left_idx] > self.heap_arr[idx]:
+        #         self.swap(left_idx,idx)
+        #         idx = left_idx
+        #     elif right_idx < self.heap_size and self.heap_arr[right_idx] > self.heap_arr[idx]:
+        #         self.swap(right_idx,idx)
+        #         idx = right_idx
+        #     else:
+        #         return
     # time complexity: O(log n)
     def delete_from_min_heap(self):
-        if self.heap_size == 0:
+        if self.heap_size == 1:
             print("nothing to delete")
 
-        self.heap_arr[1] = self.heap_arr[self.heap_size]
+        self.heap_arr[1] = self.heap_arr[self.heap_size-1]
+        self.heap_arr.pop()
         self.heap_size -= 1
 
-        idx = 1
-        while idx < self.heap_size:
-            left_idx = 2*idx
-            right_idx = 2*idx + 1
-            if left_idx <= self.heap_size and self.heap_arr[left_idx] < self.heap_arr[idx]:
-                self.swap(left_idx,idx)
-                idx = left_idx
-            elif right_idx <= self.heap_size and self.heap_arr[right_idx] < self.heap_arr[idx]:
-                self.swap(right_idx,idx)
-                idx = right_idx
-            else:
-                return
+        # idx = 1
+        # while idx < self.heap_size:
+        #     left_idx = 2*idx
+        #     right_idx = 2*idx + 1
+        #     if left_idx < self.heap_size and self.heap_arr[left_idx] < self.heap_arr[idx]:
+        #         self.swap(left_idx,idx)
+        #         idx = left_idx
+        #     elif right_idx < self.heap_size and self.heap_arr[right_idx] < self.heap_arr[idx]:
+        #         self.swap(right_idx,idx)
+        #         idx = right_idx
+        #     else:
+        #         return
+
+        for i in range((self.heap_size//2),0,-1):
+            self.heapify_min_heap(self.heap_size,i)
+
+    def heapify_max_heap(self,n,i):
+        largest = i
+        left = 2*i
+        right = (2*i)+1
+
+        if left < n and self.heap_arr[left] > self.heap_arr[largest]:
+            largest = left
+        if right < n and self.heap_arr[right] > self.heap_arr[largest]:
+            largest = right
+
+        if largest != i:
+            self.swap(i,largest)
+            self.heapify_max_heap(n,largest)
+    def heapify_min_heap(self,n,i):
+        largest = i
+        left = 2 * i
+        right = (2 * i) + 1
+
+        if left < n and self.heap_arr[left] < self.heap_arr[largest]:
+            largest = left
+        if right < n and self.heap_arr[right] < self.heap_arr[largest]:
+            largest = right
+
+        if largest != i:
+            self.swap(i,largest)
+            self.heapify_min_heap(n, largest)
 
 
 
 
-
-
-heap1 = Heap()
+heap1 = Heap([None])
 heap1.insert_in_max_heap(20)
 heap1.insert_in_max_heap(22)
 heap1.insert_in_max_heap(10)
@@ -117,7 +149,7 @@ heap1.insert_in_max_heap(15)
 heap1.insert_in_max_heap(30)
 heap1.insert_in_max_heap(35)
 heap1.print_heap()
-min_heap = Heap()
+min_heap = Heap([None])
 min_heap.insert_in_min_heap(20)
 min_heap.insert_in_min_heap(22)
 min_heap.insert_in_min_heap(10)
@@ -129,3 +161,13 @@ heap1.delete_from_max_heap()
 heap1.print_heap()
 min_heap.delete_from_min_heap()
 min_heap.print_heap()
+
+arr = [None,54,53,55,52,50]
+heap3 = Heap(arr)
+for i in range((len(heap3.heap_arr)//2),0,-1):
+    heap3.heapify_max_heap(len(heap3.heap_arr),i)
+heap3.print_heap()
+
+for i in range((len(heap3.heap_arr)//2),0,-1):
+    heap3.heapify_min_heap(len(heap3.heap_arr),i)
+heap3.print_heap()
--- Heap/readme.md
+++ Heap/readme.md
@@ -11,7 +11,19 @@
   - Assumption: the root is counted as idx 1 and array idx starts with 1 instead of 0.
   1. if node is at ith idx in array then left child is at 2*i idx and right child is at 2*i+1.
   2. if a node is at ith idx then its parent lies at int(i/2) idx in array.
+  - Assumption: the root is counted as idx 0 and array idx starts with 0.
+  1. if node is at ith idx in array then left child is at (2*i)+1 idx and right child is at 2*i+2.
+  2. if a node is at ith idx then its parent lies at int(i/2) idx in array.
 
 ## deletion in heap:
 - in case of heap deletion means delete the root.
-- 
+
+## heapify algorithm:
+- In a complete binary tree:
+  - leaf nodes lie from ((n/2)+1)th index to nth index.
+  - leaf node can be assumed to be a heap in itself.
+  - from 0 to (n/2)th index, do heapify to convert into max or min heap.
+    - Algo:
+      - assign i as largest and find the left node idx and right node idx.
+      - check condition for left and right nodes and update values of largest
+      - if value of largest has been chnages then it would not be equal to i and hence call heapify for rest of the elements.
