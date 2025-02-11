049caaeb911e90b5afe2d49054229c34741ee45d
tanya Kansal <tanyakansal@tanyas-MacBook-Pro.local>
2025-02-11 13:00:34 +0530

    q1283 solution

---
+++ companyQuestions/LinkedIn/OnlineAssessment/q1.py
@@ -0,0 +1,6 @@
+"""
+the cost of stock on each day is given in an array arr.
+An investor wants to buy the stocks in triplets suchj that sum of the cost for three days is divisible by d.
+the goal is to find the number of distinct triplets (i,j,k) such that i<j<k and the sum (arr[i]+arr[j]+arr[k]) is divisible by d.
+
+"""
